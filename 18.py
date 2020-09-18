a67b6d5cd241935303b6e5f35b50b5f25d0e4511
st2257st2257 <68289534+st2257st2257@users.noreply.github.com>
2020-09-18 12:10:03 +0300

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,61 @@
+from random import *
+import turtle
+
+
+number_of_turtles = 10
+steps_of_time_number = 1000
+
+
+
+a,b = 200,200
+turtle.penup()
+turtle.goto(a,b)
+turtle.pendown()
+turtle.goto(a,-b)
+turtle.goto(-a,-b)
+turtle.goto(-a,b)
+turtle.goto(a,b)
+turtle.penup()
+
+
+
+
+pool = [turtle.Turtle(shape='turtle') for i in range(number_of_turtles)]
+speed = [ random()*10  for i in range(number_of_turtles*2) ]
+for unit in pool:
+    unit.penup()
+    unit.speed(500)
+    unit.shape('circle')
+    unit.goto(randint(-200, 200), randint(-200, 200))
+
+
+for i in range(steps_of_time_number):
+    for counter, unit in enumerate(pool):
+        X, Y = unit.position()
+        if (abs(X) > 200):
+            speed[2 * counter] *= (-1)
+        if (abs(Y) > 200):
+            speed[2 * counter+1] *= (-1)
+
+        if (abs(X) - 200 > 10):
+            unit.goto(0,-180)
+        if (abs(Y) - 200 > 10):
+            unit.goto(0, -180)
+
+        X, Y = unit.position()
+        #acceleration
+        Fx,Fy = 0
+        for counter_n, unit_n in enumerate(pool):
+            X_n, Y_n = unit_n.position()
+            if (counter_n != counter):
+                a,b,c = 
+                F += 1/((X-X_n)**2 + (Y-Y_n)**2)
+
+
+
+
+        #speed
+
+        speed[2 * counter + 1]  -= 1
+
+        unit.goto(X+speed[2 * counter],Y+speed[2 * counter+1])
